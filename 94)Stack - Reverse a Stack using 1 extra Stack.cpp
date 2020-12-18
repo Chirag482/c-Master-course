bc3252a9b37de9339bada99175de649ac7650e64
Chirag <chiragvalecha97@gmail.com>
2020-12-18 16:59:26 +0530

    Stacks -Data structure

---
+++ 94)Stack - Reverse a Stack using 1 extra Stack.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+void transfer(stack<int> &s1,stack<int> &s2,int n){
+    for(int i=0;i<n;i++){
+        s2.push(s1.top());
+        s1.pop();
+    }
+}
+void reversestack(stack<int> &s1){
+    stack<int> s2;
+    int n = s1.size();
+
+    for(int i=0;i<n;i++){
+        //pick the element at top and insert it at the bottom
+        int x = s1.top();
+        s1.pop();
+
+        //transfer n-i-1 from stack s1 into s2
+        transfer(s1,s2,n-i-1);
+        //insert the element x in s1
+        s1.push(x);
+
+        //transfer n-i-1 elements from s2 to s1
+        transfer(s2,s1,n-i-1);
+    }
+}
+int main()
+{
+    stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+
+    reversestack(s);
+
+    while(!s.empty()){
+        cout<<s.top()<<endl;
+        s.pop();
+    }
+    return 0;
+}
